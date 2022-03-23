8fc6918fa69e5cba961e5749d5a8f19346e8b4ab
Kaspyy <50270928+Kaspyy@users.noreply.github.com>
2022-03-23 13:04:59 +0100

    Updates

---
+++ precache-manifest.d7185ccfece4b9f2dfaacae6e968ae38.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a463d5fb9229ce788413",
+    "url": "/pokedex-app/static/css/main.94ef3ba2.chunk.css"
+  },
+  {
+    "revision": "a463d5fb9229ce788413",
+    "url": "/pokedex-app/static/js/main.a463d5fb.chunk.js"
+  },
+  {
+    "revision": "aea4a5bdfab3b8beece5",
+    "url": "/pokedex-app/static/js/1.aea4a5bd.chunk.js"
+  },
+  {
+    "revision": "fd81f0edeefa18338451",
+    "url": "/pokedex-app/static/css/2.3e4755ea.chunk.css"
+  },
+  {
+    "revision": "fd81f0edeefa18338451",
+    "url": "/pokedex-app/static/js/2.fd81f0ed.chunk.js"
+  },
+  {
+    "revision": "4f45651502952ea1605e",
+    "url": "/pokedex-app/static/js/runtime~main.4f456515.js"
+  },
+  {
+    "revision": "3cfd5e85cd2597eb00d7f5b0139d3b6d",
+    "url": "/pokedex-app/static/media/Pokemon_logo.3cfd5e85.svg"
+  },
+  {
+    "revision": "49fdd2d08ff0aa76a534226511fb1c8f",
+    "url": "/pokedex-app/static/media/spinner.49fdd2d0.gif"
+  },
+  {
+    "revision": "71c3dbe3e488713f737418a2f895fefa",
+    "url": "/pokedex-app/index.html"
+  }
+];
